f7e01b4b153c5a596aa0e6337de056695b1bcf69
landor <landor@aught.org>
2014-03-15 20:46:56 -0400

    match xdebug output

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ matchcommand
 
 A rough chrome extension to look for text on pages and a php native messaging host to run commands based on matches.
 
-Currently this just looks for file:line output in kint and opens the file to the line number in sublime text.
-If anyone cares it could easily be generalized to do custom matching and different local commands.
+All the behavior is hard coded. If anyone cares it could easily be generalized to do custom matching and local commands.
+
+This will currently match:
+ * modified kint output
+ * xdebug output
+ It turns filenames into links which open the file to the indicated line in my editor, Sublime Text.
 
 Here are the changes to kint that it expects:
 ```html
--- chrome/js/content.js
+++ chrome/js/content.js
@@ -1,4 +1,17 @@
 jQuery(document).ready(function($){
+
+  /* BEGIN match xdebug output */
+
+  // mangle xdebug to get picked up by krumo format
+  $('.xdebug-error th').each(function(){
+    console.info(this.innerHTML);
+    this.innerHTML = this.innerHTML.replace(/in (.+) on line <i>([0-9]+)<\/i>/g, "in <span class=\"krumo-callee-file\" data-filename=\"$1\" data-linenumber=\"$2\">$1</span> on line <i>$2</i>");
+  });
+  
+  /* END match xdebug output */
+
+  /* BEGIN match krumo output */
+
   $('.krumo-callee-file').css({
     'cursor': 'pointer',
     'text-decoration': 'underline'
@@ -6,15 +19,17 @@ jQuery(document).ready(function($){
   $('body').on('click', '.krumo-callee-file', function(){
     var filename = this.dataset['filename'];
     var linenumber = this.dataset['linenumber'];
-    console.info(filename, linenumber);
 
     chrome.runtime.sendMessage(
       {filename: filename, linenumber: linenumber},
       function(response) {
-        console.log(response);
+        // console.log(response);
       }
     );
   });
-
+  // trigger on load for debug
   // $('.krumo-callee-file').first().click();
+  
+  /* END match krumo output */
+
 });
